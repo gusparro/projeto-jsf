ab1da021cb77a511cff7fcf7d7e8229b48b1d46f
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-02-07 10:46:51 -0300

    Ajustando equals and  hashCode da classe DefaultEntity.

--- src/main/java/com/gusparro/projeto_jsf/models/DefaultEntity.java
+++ src/main/java/com/gusparro/projeto_jsf/models/DefaultEntity.java
@@ -9,12 +9,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Getter
 @Setter
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @MappedSuperclass
 public class DefaultEntity {
 
@@ -26,5 +26,18 @@ public class DefaultEntity {
     protected LocalDateTime createdAt;
 
     protected LocalDateTime updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DefaultEntity that = (DefaultEntity) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 
 }
