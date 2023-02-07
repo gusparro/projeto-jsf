d6c6254e69d08bfcfd139c7d835fac61009307d5
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-02-07 11:40:37 -0300

    Criando arquivo para popular o banco de dados.

---
+++ src/main/resources/META-INF/database/init.sql
@@ -0,0 +1,7 @@
+INSERT INTO AppUser (name, email, password) VALUES ('Gustavo Parro', 'parrogustavo@gmail.com', '123456');
+
+INSERT INTO Role (name, description) VALUES ('Admin', 'Person who is in charge of the operation of the website.');
+INSERT INTO Role (name, description) VALUES ('Manager', 'A person responsible for controlling  all or part of the website.');
+
+
+
