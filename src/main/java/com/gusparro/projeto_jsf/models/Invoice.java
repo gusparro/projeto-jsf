466862cd0cac584795500389e78dd7222ae7ead3
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-02-09 15:16:37 -0300

    Atualizando o atributo xml da classe Invoice para receber arquivos.

--- src/main/java/com/gusparro/projeto_jsf/models/Invoice.java
+++ src/main/java/com/gusparro/projeto_jsf/models/Invoice.java
@@ -2,6 +2,7 @@ package com.gusparro.projeto_jsf.models;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
 import jakarta.persistence.OneToOne;
 import lombok.*;
 
@@ -13,7 +14,8 @@ import lombok.*;
 @Entity
 public class Invoice extends DefaultEntity {
 
-    private String xml;
+    @Lob
+    private byte[] xml;
 
     @OneToOne(optional = false)
     @JoinColumn(name = "order_id")
