e268a15cb97c5bcfb9b124895dacce2acc3822b8
Gustavo Ferreira Parro <gustavoparro.dev@gmail.com>
2023-02-12 21:48:14 -0300

    Criando queries com JPQL na classe AppUserRepositoryJPQL.

---
+++ src/main/java/com/gusparro/projeto_jsf/repositories/jpql/AppUserRepositoryJPQL.java
@@ -0,0 +1,63 @@
+package com.gusparro.projeto_jsf.repositories.jpql;
+
+import com.gusparro.projeto_jsf.models.AppUser;
+import com.gusparro.projeto_jsf.repositories.DefaultRepository;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AppUserRepositoryJPQL extends DefaultRepository<AppUser> {
+
+    private String jpql;
+
+    private AppUser appUser;
+
+    private List<AppUser> appUsers;
+
+    private TypedQuery<AppUser> query;
+
+    public Optional<AppUser> findById(Long id) {
+        jpql = "SELECT ap FROM AppUser ap WHERE ap.id = :id";
+
+        query = entityManager.createQuery(jpql, AppUser.class);
+        query.setParameter("id", id);
+
+        appUser = query.getSingleResult();
+
+        return Optional.ofNullable(appUser);
+    }
+
+    public Optional<AppUser> findByEmail(String email) {
+        jpql = "SELECT ap FROM AppUser ap WHERE ap.email = :email";
+
+        query = entityManager.createQuery(jpql, AppUser.class);
+        query.setParameter("email", email);
+
+        appUser = query.getSingleResult();
+
+        return Optional.ofNullable(appUser);
+    }
+
+    public List<AppUser> findAll() {
+        jpql = "SELECT ap FROM AppUser ap";
+
+        query = entityManager.createQuery(jpql, AppUser.class);
+
+        appUsers = query.getResultList();
+
+        return appUsers;
+    }
+
+    public List<AppUser> findAllByName(String name) {
+        jpql = "SELECT ap FROM AppUser ap WHERE UPPER(ap.name) like UPPER(CONCAT('%', :name, '%'))";
+
+        query = entityManager.createQuery(jpql, AppUser.class);
+        query.setParameter("name", name);
+
+        appUsers = query.getResultList();
+
+        return appUsers;
+    }
+
+}
